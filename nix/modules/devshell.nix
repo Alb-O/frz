84f32f76f4cc2d3494e31f2d73cd44b17ce6575d
Albert O'Shea <albertoshea2@gmail.com>
2025-10-07 16:55:24 +1100

    Add nix/rust template files

---
+++ nix/modules/devshell.nix
@@ -0,0 +1,25 @@
+{ inputs, ... }:
+{
+  perSystem =
+    { config
+    , self'
+    , pkgs
+    , lib
+    , ...
+    }:
+    {
+      devShells.default = pkgs.mkShell {
+        name = "riz-shell";
+        inputsFrom = [
+          self'.devShells.rust
+          config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
+        ];
+        packages = with pkgs; [
+          just
+          nixd # Nix language server
+          bacon
+          cargo-edit
+        ];
+      };
+    };
+}
---
+++ nix/modules/rust.nix
@@ -0,0 +1,29 @@
+{ inputs, ... }:
+{
+  imports = [
+    inputs.rust-flake.flakeModules.default
+    inputs.rust-flake.flakeModules.nixpkgs
+  ];
+  perSystem =
+    { config
+    , self'
+    , pkgs
+    , lib
+    , ...
+    }:
+    {
+      rust-project.crates."riz".crane.args = {
+        buildInputs = lib.optionals pkgs.stdenv.isDarwin (
+          with pkgs.darwin.apple_sdk.frameworks;
+          [
+            IOKit
+          ]
+        );
+      };
+      # Ensure the path for the top-level crate is set so default-crates logic
+      # doesn't try to read an undefined path (this fixes errors when the crate
+      # is referenced by the rust-flake module).
+      rust-project.crates."riz".path = ../../.;
+      packages.default = self'.packages.riz;
+    };
+}
